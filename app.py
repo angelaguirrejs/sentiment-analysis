a881679f69f5811e4ccb1637ab5980d42bb2c5a1
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-05-22 22:12:01 -0500

    root route

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from ml_model import *
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def hi():
+    return jsonify({"message" : "Hey there!"})
 
 @app.route('/predict', methods = ['POST'])
 def predict():
@@ -10,4 +15,5 @@ def predict():
     return jsonify({"sentiment" : res})
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 4000)
+    port = 8000
+    app.run(debug=True, port=port)
